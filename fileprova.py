3083e157f30f5d31e7fed74e409db15d28137bd9
Sveva <pepe.sveva@gmail.com>
2019-12-19 12:36:10 +0100

    fix with functions

--- fileprova.py
+++ fileprova.py
@@ -8,18 +8,18 @@ import speech_recognition as sr
 
 
 engine = pyttsx3.init()
-
 voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[2].id)
+engine.setProperty('voice', voices[2].id) # voice set
 volume = engine.getProperty('volume')   
-engine.setProperty('volume', 10.0)
+engine.setProperty('volume', 10.0) # volume of voice
 rate = engine.getProperty('rate')
 engine.setProperty('rate', 125)   # voice rate that you want to set
 
-greetings = ['hey there', 'hello', 'hi', 'Hai', 'hey!', 'hey']
+greetings = ['hey there', 'hello', 'hi', 'hey!', 'hey']
 question = ['how are you', 'how are you doing']
 responses = ['Okay', "I'm fine"]
 var1 = ['who made you', 'who created you']
+var11 = ['i am a bot', 'i am you father, Luke']
 var2 = [ ' I was made by Sveva', ' I was made by Simone', ' I was made by Marco']
 var3 = ['what time is it', 'what is the time', 'time']
 var4 = ['who are you', 'what is you name']
@@ -31,85 +31,89 @@ cmd4 = ['open youtube', 'i want to watch a video']
 cmd6 = ['exit', 'close', 'goodbye', 'nothing']
 cmd7 = ['what is your color', 'what is your colour', 'your color', 'your color?']
 colrep = ['Right now its rainbow', 'Right now its transparent', 'Right now its non chromatic']
-cmd8 = ['what is you favourite colour', 'what is your favourite color']
 cmd9 = ['thank you']
 repfr9 = ['youre welcome', 'glad i could help you']
 
-while True:
-    now = datetime.datetime.now()
+
+def myCommand():
     r = sr.Recognizer()
     with sr.Microphone() as source:
-        print("Tell me something:")
+        print('Say something...')
+        r.pause_threshold = 1
+        r.adjust_for_ambient_noise(source, duration=1)
         audio = r.listen(source)
         try:
-            print("You said:- " + r.recognize_google(audio))
+            command = r.recognize_google(audio).lower()
+            print('You said: ' + command + '\n')
+            skills(command)
+        #loop back to continue to listen for commands if unrecognizable speech is received
         except sr.UnknownValueError:
-            print("Could not understand audio")
+            print('Pippo : ....')
             engine.say('I didnt get that. Rerun the code')
-
             engine.runAndWait()
-    if r.recognize_google(audio) in greetings:
-        random_greeting = random.choice(greetings)
-        print(random_greeting)
-        engine.say(random_greeting)
-        engine.runAndWait()
-    elif r.recognize_google(audio) in question:
-        engine.say('I am fine')
-        engine.runAndWait()
-        print('I am fine')
-    elif r.recognize_google(audio) in var1:
-        engine.say(var2)
-        engine.runAndWait()
+            command = myCommand()
+
+def PippoResponse(audio):
+    print('Pippo: '+ audio)
+    engine.say(audio)
+    engine.runAndWait()
+
+def skills(command):
+        
+    if command in greetings:
+        reply = random.choice(greetings)
+        PippoResponse(reply)
+        myCommand()
+    elif command in question:
+        reply = random.choice(responses)
+        PippoResponse(reply)
+        myCommand()
+    elif command in var1:
         reply = random.choice(var2)
-        print(reply)
-    elif r.recognize_google(audio) in cmd9:
-        print(random.choice(repfr9))
-        engine.say(random.choice(repfr9))
-        engine.runAndWait()
-    elif r.recognize_google(audio) in cmd7:
-        print(random.choice(colrep))
-        engine.say(random.choice(colrep))
-        engine.runAndWait()
-        print('It keeps changing every micro second')
-        engine.say('It keeps changing every micro second')
-        engine.runAndWait()
-    elif r.recognize_google(audio) in cmd8:
-        print(random.choice(colrep))
-        engine.say(random.choice(colrep))
-        engine.runAndWait()
-        print('It keeps changing every micro second')
-        engine.say('It keeps changing every micro second')
-        engine.runAndWait()
-    elif r.recognize_google(audio) in cmd2:
+        PippoResponse(reply)
+        myCommand()
+    elif command in cmd9:
+        reply = random.choice(repfr9)
+        PippoResponse(reply)
+        myCommand()
+    elif command in cmd7:
+        reply = random.choice(colrep)
+        PippoResponse(reply)
+        myCommand()
+    elif command in cmd2:
         mixer.init()
         mixer.music.load("harvard.wav")
         mixer.music.play(0)
-    elif r.recognize_google(audio) in var4:
-        engine.say('I am a bot')
-        engine.runAndWait()
-    elif r.recognize_google(audio) in cmd4:
+        myCommand()
+    elif command in var4:
+        reply = random.choice(var11)
+        PippoResponse(reply)
+        myCommand()
+    elif command in cmd4:
         webbrowser.open('www.youtube.com')
-    elif r.recognize_google(audio) in cmd6:
-        print('see you later')
-        engine.say('see you later')
-        engine.runAndWait()
+    elif command in cmd6:
+        PippoResponse('Bye, see you later')
         exit()
-    elif r.recognize_google(audio) in var3:
+    elif command in var3:
         print("Current date and time : ")
-        print(now.strftime("The time is %H:%M"))
-        engine.say(now.strftime("The time is %H:%M"))
-        engine.runAndWait()
-    elif r.recognize_google(audio) in cmd1:
+        reply= now.strftime("The time is %H:%M")
+        PippoResponse(reply)
+        myCommand()
+    elif command in cmd1:
         webbrowser.open('www.google.com')
-    elif r.recognize_google(audio) in cmd3:
-        jokrep = random.choice(jokes)
-        engine.say(jokrep)
-        engine.runAndWait()
+    elif command in cmd3:
+        reply = random.choice(jokes)
+        PippoResponse(reply)
+        myCommand()
     else:
         engine.say("please wait")
         engine.runAndWait()
-        print(wikipedia.summary(r.recognize_google(audio)))
-        engine.say(wikipedia.summary(r.recognize_google(audio)))
+        PippoResponse("please wait")
+        print(wikipedia.summary(command))
+        engine.say(wikipedia.summary(command))
         engine.runAndWait()
-        userInput3 = input("or else search in google")
-        webbrowser.open_new('www.google.com/search?q=' + userInput3)
+        myCommand()
+
+
+if __name__ == '__main__':
+    myCommand()
